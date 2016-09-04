150516815c84a0a11c8678daa67815a74d4f8311
RafPe <rafal@pieniazek.nl>
2016-09-04 15:55:50 +0200

    Add generated cookbook content

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'kvm'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures kvm'
+long_description 'Installs/Configures kvm'
+version '0.1.0'
