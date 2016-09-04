53601a7bab0f19f4a6737e607db7a4496d2bfa27
RafPe <rafal@pieniazek.nl>
2016-09-04 16:39:37 +0200

    Created initial cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,25 @@
 # Cookbook Name:: kvm
 # Recipe:: default
 #
-# Copyright (c) 2016 The Authors, All Rights Reserved.
+# Copyright (c) 2016 RafPe, All Rights Reserved.
+
+bash "yum groupinstall Development tools" do
+    user "root"
+    group "root"
+    code <<-EOC
+      yum groupinstall "Development tools" -y
+    EOC
+    not_if "yum grouplist installed | grep 'Development tools'"
+  end
+
+%w(kvm virt-manager libvirt virt-install bridge-utils qemu-kvm libguestfs-tools genisoimage).each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+%w(xorg-x11-fonts-Type1 xorg-x11-fonts-misc python-pip python-bottle python-argparse python-jinja2 xauth dejavu-lgc-sans-fonts xorg-x11-xauth htop wget vim ntpdate).each do |pkg|
+  package pkg do
+    action :install
+  end
+end
